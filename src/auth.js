c70e0a4ebba9a971bf18c38fb6623576bb1959b3
anzarmalik <anzarmalikcse@gmail.com>
2021-09-01 01:52:07 +0530

    added basic  authorization invite mutation

--- src/auth.js
+++ src/auth.js
@@ -31,7 +31,11 @@ const getUserFromToken = token => {
  * @param {Function} next next resolver function ro run
  */
 const authenticated = next => (root, args, context, info) => {
-  throw new Error("something is   going on... ");
+  if(!context.user){
+  throw new Error(" Not Authenticated (: ");
+  }
+
+  return next(root, args, context, info);
 }
 
 /**
@@ -41,7 +45,11 @@ const authenticated = next => (root, args, context, info) => {
  * @param {Function} next next resolver function to run
  */
 const authorized = (role, next) => (root, args, context, info) => {
-  
+  if(context.user.role !== role){
+    throw new Error(" Not Authorized :) ");
+  }
+
+  return next(root, args, context, info);
 }
 
 module.exports = {
--- src/resolvers.js
+++ src/resolvers.js
@@ -42,9 +42,9 @@ module.exports = {
       return models.User.updateOne({id: user.id}, input)
     },
     // admin role
-    invite(_, {input}, {user}) {
+    invite: authenticated(authorized('ADMIN',(_, {input}, {user})=>{
       return {from: user.id, role: input.role, createdAt: Date.now(), email: input.email}
-    },
+    })),
 
     signup(_, {input}, {models, createToken}) {
       const existing = models.User.findOne({email: input.email})
